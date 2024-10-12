080c8ae2ede983cc58cf25ccaef25411ad842725
Raulzadaa <rrnas27@gmail.com>
2024-10-12 11:46:11 -0300

    Initial commit

---
+++ Median_of_Two_Sorted_Arrays.py
@@ -0,0 +1,30 @@
+class Solution(object):
+
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+
+        mergedArray = []
+
+        for c in nums1:
+            mergedArray.append(c)
+
+        for c in nums2:
+            mergedArray.append(c)
+
+        size = len(mergedArray)
+        mergedArray = sorted(mergedArray)
+
+        if len(mergedArray) % 2 == 0:
+            a = float(mergedArray[(size//2)])
+            b = float(mergedArray[(size//2) - 1 ])
+
+            median = (a+b)/2
+
+        else:
+            median = mergedArray[(size//2) ]
+
+        return float(median)    
